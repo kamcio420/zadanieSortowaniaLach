220e3fe15cf53df1f2c8882a9d50c564f4ba5ea8
kamillachdev <kamillachdev@gmail.com>
2022-12-10 12:20:14 +0100

    Initial commit and Layout

---
+++ app/src/main/java/com/example/sortowanialach/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.sortowanialach
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+    }
+}
