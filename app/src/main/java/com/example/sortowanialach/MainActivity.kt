7f2a0d470e7039cc3648ac2e58b665083f02491f
kamillachdev <kamillachdev@gmail.com>
2022-12-10 12:28:18 +0100

    Bubble sorting

--- app/src/main/java/com/example/sortowanialach/MainActivity.kt
+++ app/src/main/java/com/example/sortowanialach/MainActivity.kt
@@ -25,5 +25,24 @@ class MainActivity : AppCompatActivity() {
         val s5 = findViewById<TextView>(R.id.scalanieTime)
         val sortResultOutput = findViewById<TextView>(R.id.sortResult)
 
+        //BUBBLE SORTING
+        fun bubbleSorting(arr:ArrayList<Int>)
+        {
+            var swap = true
+            while(swap)
+            {
+                swap = false
+                for(i in 0 until arr.size-1)
+                {
+                    if(arr[i] > arr[i+1])
+                    {
+                        val temp = arr[i]
+                        arr[i] = arr[i+1]
+                        arr[i + 1] = temp
+                        swap = true
+                    }
+                }
+            }
+        }
     }
 }
