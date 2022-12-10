c937b3ca7796ce2585e4a18076ef129a83cff45d
kamillachdev <kamillachdev@gmail.com>
2022-12-10 12:25:37 +0100

    Declarations

--- app/src/main/java/com/example/sortowanialach/MainActivity.kt
+++ app/src/main/java/com/example/sortowanialach/MainActivity.kt
@@ -13,6 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        //DECRALATIONS
+        val sortButton = findViewById<Button>(R.id.sortButton)
+        val times = findViewById<TextInputEditText>(R.id.timesCount)
+        val elements = findViewById<TextInputEditText>(R.id.elementsCount)
+        val s1 = findViewById<TextView>(R.id.wstawianieTime)
+        val s2 = findViewById<TextView>(R.id.babelkoweTime)
+        val s3 = findViewById<TextView>(R.id.szybkieTime)
+        val s4 = findViewById<TextView>(R.id.heapsortTIme)
+        val s5 = findViewById<TextView>(R.id.scalanieTime)
+        val sortResultOutput = findViewById<TextView>(R.id.sortResult)
+
     }
 }
